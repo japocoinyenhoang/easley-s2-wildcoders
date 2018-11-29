b0898108dbe22ea6559fe9f71fe2af0aa9fa6c0e
JapocoinYenHoang <japocoin@gmail.com>
2018-11-29 10:51:56 +0100

    ejemploevent

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('>> Ready :)');
+const job = document.querySelector('#job');
+const eventExample = document.querySelector('.event__example');
+
+const fillField = (event) => {
+   const guilty = event.target;
+   eventExample.innerHTML = guilty.value;
+}
+
+job.addEventListener('keyup', fillField);
