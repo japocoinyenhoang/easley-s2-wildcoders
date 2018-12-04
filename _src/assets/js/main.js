ce25c0dc7ea65816a2ca84f0fc362412ad0e1b3a
babelarr <babelarr@gmail.com>
2018-12-04 21:34:13 +0100

    do radios

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -20,30 +20,70 @@ function fillJob(event){
 
 job.addEventListener('keyup', fillJob);
 
+
+
 //Paleta de colores
-const colors = document.querySelectorAll('.radio_button--color');
-const contactIcons = document.querySelector('.contact-icons');
-const cardContact = document.querySelectorAll('.card__contact');
-//const bgSkills = document.querySelector('.card__skills-item');
+const colors = document.querySelectorAll('.radio__button--color');
+
+//Objeto HTML que cambia de color
+const cardData = document.querySelector('.card__data');
+
+//Listas de objetos HTML que cambian de color
+const contactIcons = document.querySelectorAll('.contact-icons');
+const skillsTags = document.querySelectorAll('.card__skills-item');
 
 function applyPalette(e) {
-  const p = e.currentTarget.value;
+  const p = parseInt(e.currentTarget.value);
   console.log('>',p);
   
   if (p === 1) {
-    // paleta por defecto
-    cardContact.classList.remove('contact-icons__color2', 'contact-icons__color3');
+    /* En el caso del cardData, lo estamos manejando como un objeto HTML.
+    Igual que en el ejemplo CodePen de Carlos */
+
+    //Objeto cardData
+    cardData.classList.add('card-data__color1');
+    cardData.classList.remove('card-data__color2', 'card-data__color3');
+    
+    /* Pero en el caso de los iconos y los skills, estos tienen que ser manejados como
+    listas de objetos HTML. Por eso usamos "querySelectorAll" en lugar de "querySelector" y
+    hacemos un "for" para aplicar los cambios a todos sus elementos */
+
+    //Lista de Iconos
+    for (const icon of contactIcons) {
+      icon.classList.add('contact-icons__color1');
+      icon.classList.remove('contact-icons__color2', 'contact-icons__color3');
+    }
+
+    //Lista de Tags
+    for (const tag of skillsTags) {
+      tag.classList.add('card__skills-item__color1');
+      tag.classList.remove('card__skills-item__color2', 'card__skills-item__color3');
+    }
   } else if (p === 2) {
-    // paleta 2
-    card.classList.add('contact-icons__color2');
-    card.classList.remove('contact-icons__color1', 'contact-icons__color3');
+    cardData.classList.add('card-data__color2');
+    cardData.classList.remove('card-data__color1', 'card-data__color3');
+    for (const icon of contactIcons) {
+      icon.classList.add('contact-icons__color2');
+      icon.classList.remove('contact-icons__color1', 'contact-icons__color3');
+    }
+    for (const tag of skillsTags) {
+      tag.classList.add('card__skills-item__color2');
+      tag.classList.remove('card__skills-item__color1', 'card__skills-item__color3');
+    }
   } else {
-    // paleta 3
-    card.classList.add('contact-icons__color3');
-    card.classList.remove('contact-icons__color1', 'contact-icons__color2');
+    cardData.classList.add('card-data__color3');
+    cardData.classList.remove('card-data__color1', 'card-data__color2');
+    for (const icon of contactIcons) {
+      icon.classList.add('contact-icons__color3');
+      icon.classList.remove('contact-icons__color1', 'contact-icons__color2');
+    }
+    for (const tag of skillsTags) {
+      tag.classList.add('card__skills-item__color3');
+      tag.classList.remove('card__skills-item__color1', 'card__skills-item__color2');
+    }
   }
 }
-
+// En este caso, no se porque... no me funciona con 'click' y si con 'change'
 for (const c of colors) {
-  c.addEventListener('click', applyPalette);
+  c.addEventListener('change', applyPalette);
 }
