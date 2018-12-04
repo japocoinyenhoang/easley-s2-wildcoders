cd116a06d87886fe0acd94c99ffcefec6d15d668
Ester Legaz <esterlegaz@gmail.com>
2018-12-04 17:14:40 +0100

    Test JS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -19,3 +19,31 @@ function fillJob(event){
 }
 
 job.addEventListener('keyup', fillJob);
+
+//Paleta de colores
+const colors = document.querySelectorAll('.radio_button--color');
+const contactIcons = document.querySelector('.contact-icons');
+const cardContact = document.querySelectorAll('.card__contact');
+//const bgSkills = document.querySelector('.card__skills-item');
+
+function applyPalette(e) {
+  const p = e.currentTarget.value;
+  console.log('>',p);
+  
+  if (p === 1) {
+    // paleta por defecto
+    cardContact.classList.remove('contact-icons__color2', 'contact-icons__color3');
+  } else if (p === 2) {
+    // paleta 2
+    card.classList.add('contact-icons__color2');
+    card.classList.remove('contact-icons__color1', 'contact-icons__color3');
+  } else {
+    // paleta 3
+    card.classList.add('contact-icons__color3');
+    card.classList.remove('contact-icons__color1', 'contact-icons__color2');
+  }
+}
+
+for (const c of colors) {
+  c.addEventListener('click', applyPalette);
+}
