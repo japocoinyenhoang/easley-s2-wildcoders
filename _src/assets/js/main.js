292442fae2dc6f8e3ff740ea1e7307239a8fc43b
JapocoinYenHoang <japocoin@gmail.com>
2018-12-04 07:06:29 +0100

    SOS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -25,11 +25,23 @@ job.addEventListener('keyup', fillJob);
 
 
 // Añadir imagen
-const selectedImage =document.getElementById('btn__add-img');
-const destinationImage =document.getElementById('card__img--profile');
-// al pulsar en el boton me abra el explorador de archivos
 
+// function init(){
+//   let selectedImage = document.getElementById('btn__add-img');
+//   selectedImage.addEventListener('change', mostrarImagen, false);
+// }
 
+// function mostrarImagen(event) {
+//   let file = event.target.files[0];
+//   let reader = new FileReader();
+//   reader.onload = function(event){
+//     let destinationImage =document.getElementById('card__img--profile');
+//     destinationImage.src= event.target.result;
+//   }
+//   reader.readAsDataURL(file);
+// }
+
+// window.addEventListener('load', init, false);
 
 // esto solo va con jquery
 // selectedImage.change(function(){
@@ -52,53 +64,79 @@ const destinationImage =document.getElementById('card__img--profile');
 // cuando seleccione la foto que la coloque en el recuadro de al lado, y en el card-editor
 
 
-// telefono, mail, linkedin, github
-let email = document.getElementById('email');
-//On Input Change Events
-email.addEventListener('input', function () {
-   if (email.value.length == 0) {
-       document.getElementById('card__icon--mail').innerHTML = 'pepa pig';
-   } else {
-       document.getElementById('card__icon--mail').innerHTML = this.value;
-   }
-});
-
-
-//Seleccionar Skills
-
-// tengo tres botones, 
-// por defecto aparecen las tres
-// cuadno selecciono cualquiera de ellas desaparecen todas y aparece solo la que he cliqueado o las que he cliqueado en orden
-
-// de donde las cojo
-const htmlSkill=document.getElementById('skills__data--html');
-const cssSkill=document.getElementById('skills__data--css');
-const gulpSkill=document.getElementById('skills__data--gulp');
-
-// donde estan sus iconos
-const htmlIcon = document.querySelector('.card__skills--html');
-const cssIcon = document.querySelector ('.card__skills--css');
-const gulpIcon = document.querySelector ('.card__skills--gulp');
-
-
-function fillSkills(event){
-  const SkillField = event.currentTarget;
-  card__skills.innerHTML = SkillFiel.value;
-}
-
-htmlSkill.addEventListener('keyup', fillSkills);
-
-
-// const skills=[htmSkill, cssSkill, gulpSkill];
-
-// function deleteArrowItem (){
-//   let checkedValue = document.querySelector ('.message__checkbox:checked').value;
-//   checkedValue =null;
-//   let inputElements = document.getElementsByClassName('message__checkbox');
-//   skills.length=0;
-//   for(let i=0; skills.length<=3; ++i){
-//     if(htmlSkill.value==checked){
-//       card__skills.innerHTML (htmlIcon);
+// // telefono, mail, linkedin, github
+// let email = document.getElementById('email');
+// //On Input Change Events
+// email.addEventListener('input', function () {
+//    if (email.value.length == 0) {
+//        document.getElementById('card__icon--mail').innerHTML = 'pepa pig';
+//    } else {
+//        document.getElementById('card__icon--mail').innerHTML = this.value;
 //    }
+// });
+
+
+// //Seleccionar Skills
+
+// // tengo tres botones, 
+// // por defecto aparecen las tres
+// // cuadno selecciono cualquiera de ellas desaparecen todas y aparece solo la que he cliqueado o las que he cliqueado en orden
+
+// // de donde las cojo
+// const htmlSkill=document.getElementById('skills__data--html');
+// const cssSkill=document.getElementById('skills__data--css');
+// const gulpSkill=document.getElementById('skills__data--gulp');
+
+// // donde estan sus iconos
+// const htmlIcon = document.querySelector('.card__skills--html');
+// const cssIcon = document.querySelector ('.card__skills--css');
+// const gulpIcon = document.querySelector ('.card__skills--gulp');
+
+
+
+
+
+
+// function fillSkills(event){
+//   const SkillField = event.currentTarget;
+//   card__skills.innerHTML = SkillFiel.value;
+// }
+
+// htmlSkill.addEventListener('keyup', fillSkills);
+
+
+// // const skills=[htmSkill, cssSkill, gulpSkill];
+// // para añadir o eliminar la clase .hidden
+// 
+// ​
+// // Si contiene la clase hidden
+// 
+//   // Elimina la clase
+//   
+// 
+//   // Añade la clase hidden
+//   
+// }
+
+
+function deleteArrowItem (){
+    const activableSection = document.querySelector('.card__skills-item');
+    if (activableSection.classList.contains('hidden')) {
+                activableSection.classList.remove('hidden');
+    } else { 
+                activableSection.classList.add('hidden');
+    }
+
+  let checkedValue = document.querySelector ('.message__checkbox:checked').value;
+  checkedValue =null;
+  let inputElements = document.getElementsByClassName('message__checkbox');
+  skills.length=0;
+  for(let i=0; skills.length<=3; ++i){
+    if(htmlSkill.value==checked){
+      card__skills.innerHTML (htmlIcon);
+      break;
+   
+    }
     
-//   }
+  }
+}
