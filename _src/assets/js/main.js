44bd51c3bab1331bc8170b91a010d3cbf7a42f67
babelarr <babelarr@gmail.com>
2018-12-13 20:33:01 +0100

    button reset works

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -69,8 +69,56 @@ const dataJason = {
   "skills": ["", "", ""]
 };
 
+//Resetear valores por defecto con botón Reset
 
-console.log(dataJason);
+const btnReset = document.querySelector('.btn__reset');
+
+const resetJason = () => {
+  cardName.innerHTML = 'Nombre y Apellidos';
+  name.value = '';
+
+  cardJob.innerHTML = 'Front end Developer';
+  job.value = '';
+
+  profileImage.style.backgroundImage = '';
+  divPreviewImage.style.backgroundImage = '';
+
+  phoneNumber.value = '';
+  contactIcons[0].classList.add('hidden');
+
+  email.value = '';
+  contactIcons[1].classList.add('hidden');
+
+  linkedin.value = '';
+  contactIcons[2].classList.add('hidden');
+
+  github.value = '';
+  contactIcons[3].classList.add('hidden');
+
+  //Paleta de colores por defecto
+  cardData.classList.add('card-data__color1');
+  cardData.classList.remove('card-data__color2', 'card-data__color3');
+  //Lista de Iconos
+  for (const icon of contactIcons) {
+    icon.classList.add('contact-icons__color1');
+    icon.classList.remove('contact-icons__color2', 'contact-icons__color3');
+  }
+  //Lista de Tags
+  for (const tag of skillsTags) {
+    tag.classList.add('card__skills-item__color1');
+    tag.classList.remove('card__skills-item__color2', 'card__skills-item__color3');
+  }
+
+  colors[0].checked = true;
+
+  //Tipografía por defecto
+  cardName.classList.remove('typo-2', 'typo-3');
+  cardJob.classList.remove('typo-2', 'typo-3');
+
+  inputRadio[0].checked = true;
+};
+btnReset.addEventListener('click', resetJason);
+
 
 function fillName(event) {
   const nameField = event.currentTarget;
@@ -163,7 +211,6 @@ const inputRadio = document.querySelectorAll('.typo__radio');
 function applyTypo(e) {
   const q = parseInt(e.currentTarget.value);
   dataJason.typography = q;
-
 
   if (q === 1) {
     // typo por defecto
