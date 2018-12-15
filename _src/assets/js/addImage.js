7517eac47a13e0ff6877ad147857f11472b68f1f
JapocoinYenHoang <japocoin@gmail.com>
2018-12-15 05:56:15 +0100

    add JS partition

---
+++ _src/assets/js/addImage.js
@@ -0,0 +1,28 @@
+'use strict';
+// Añadir imagen
+
+const fr = new FileReader();
+const uploadBtn = document.querySelector('.btn__add-img');
+const fileField = document.querySelector('#btn__add-img');
+const profileImage = document.querySelector('.card__img');
+const divPreviewImage = document.querySelector('.square__img');
+
+function getImage(e) {
+  const myFile = e.currentTarget.files[0];
+  fr.addEventListener('load', writeImage);
+  fr.readAsDataURL(myFile);
+  dataJason.photo = myFile;
+
+}
+
+function writeImage() {
+  profileImage.style.backgroundImage = `url(${fr.result})`;
+  divPreviewImage.style.backgroundImage = `url(${fr.result})`;
+}
+
+function fakeFileClick() {
+  fileField.click();
+}
+
+fileField.addEventListener('change', getImage);
+uploadBtn.addEventListener('click', fakeFileClick);
